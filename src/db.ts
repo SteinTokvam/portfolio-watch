d5334fb41f5780d7a17f1b32e18b3319e352a6aa
Stein Petter Tokvam <stto1210@gmail.com>
2025-03-07 14:45:10 +0100

    remove init function

--- src/db.ts
+++ src/db.ts
@@ -55,16 +55,6 @@ function initDb() {
     `);
 }
 
-function initAccounts() {
-    insertAccount("2024-09-16 06:19:56.305148+00","Kron","Aksjesparekonto",55003.5448741145,true,JSON.stringify({access_key: "67faed48-2b2f-42c4-ae3e-12f1d78b8708", account_key: "ff16ab6d-159a-4c94-908a-6606370f4570"}))
-    insertAccount("2025-02-06 11:14:55.158159+00","FundingPartner","Obligasjon",8000,true,JSON.stringify({username: "stto1210@gmail.com",password: "like8Not"}))
-    insertAccount("2024-06-06 11:28:46.718726+00","Nordnet","Aksjesparekonto",23538.799483462484,false,null)
-    insertAccount("2024-11-06 07:16:55.51106+00","Tangem","Kryptovaluta",9267.7596026015,false,null)
-    insertAccount("2024-06-06 11:28:46.71627+00","Folkeinvest","Aksjefondskonto",2000,false,null)
-    insertAccount("2024-08-27 13:59:06.445019+00","Bare Bitcoin","Kryptovaluta",4664.8327240515,true,JSON.stringify({access_key:"ota_OTI5ZDK5M2UTM2I5YS0ZODJLLTLMYJMTYWJLMGZLNDK4Y2FM"}))
-}   
-
-
 function insertAccessInfo(created_at: string, account_id: number, access_info: AccessInfo) {
     const stmt = db.prepare('INSERT INTO access_info (created_at, account_id, access_key, account_key, username, password) VALUES (?, ?, ?, ?, ?, ?)');
     stmt.run(created_at, account_id, access_info.access_key, access_info.account_key, access_info.username, access_info.password);
